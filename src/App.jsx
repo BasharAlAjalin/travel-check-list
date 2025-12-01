f080b39fcb5dd7318667517d0cb34db6032fba98
Bashar Al-Ajalin <221134@ppu.edu.ps>
2025-12-01 23:30:25 +0200

    Apply the logic of controlled elements for Form componnents

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
@@ -18,20 +20,38 @@ function Logo() {
   return <h1>🏝️ Far Away 🧳</h1>;
 }
 function Form() {
+  const [description, setDescription] = useState("");
+  const [number, setNumber] = useState(1);
+
   function handleSubmit(e) {
     e.preventDefault();
+    if (!description) return;
+
+    const newItem = { description, number, packed: false, id: Date.now() };
+    console.log(newItem);
+
+    setDescription("");
+    setNumber(1);
   }
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip ?</h3>
-      <select>
+      <select
+        value={number}
+        onChange={(e) => setNumber(Number(e.target.value))}
+      >
         {Array.from({ length: 20 }, (_, index) => index + 1).map((number) => (
           <option value={number} key={number}>
             {number}
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Enter the item..." />
+      <input
+        type="text"
+        placeholder="Enter the item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
       <button>ADD</button>
     </form>
   );
